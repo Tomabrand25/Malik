63e09643171fea6778f45f125f9b82d05d9b7815
Tomabrand25 <malikjocker0@gmail.com>
2025-01-03 03:19:47 +0500

    Update 0_🤞RDX _Zain.js

--- models/commands/0_🤞RDX _Zain.js
+++ models/commands/0_🤞RDX _Zain.js
@@ -19,11 +19,10 @@ react.includes("arun")) {
 		var msg = {
 				body: "★꧁💗𝕄𝕐 ℍ𝔸ℕ𝔻𝕊𝕆𝕄𝔼 𝕆𝕎ℕ𝔼ℝ🥰꧂                    𝗠𝗮𝗱𝗲 𝗕𝘆🫶★\n\n✦꧁𝐙𝐚𝐢𝐧𝐢-𝐉𝐮𝐭𝐭꧂✦\n\n★★᭄𝐳𝐚𝐢𝐧 𝐤𝐚 𝐛𝐨𝐭 𝐡𝐮 𝐦𝐚𝐬𝐨𝐨𝐦 𝐬𝐚 🥺:\n\n✦ 𝘄𝗮𝘀𝗲 𝗼𝘄𝗻𝗲𝗿 𝗴 𝘃𝗶𝗱𝗲𝗼 𝗯𝗿𝗶 𝗹𝗴𝗮𝗲𝘆🙈🔥  ✦ \n𝗡𝗢𝗧𝗘 \n 𝙱𝙾𝚃 𝙼𝙰 𝙺𝙸𝚂𝙸 𝙱𝙷𝙸 𝙺𝙸𝚂𝙼 𝙺𝙰 𝙸𝚂𝚂𝚄𝙴 𝚈𝙰 𝙰𝙿𝙽𝙴 𝙽𝙰𝙼𝙴 𝙺𝙰 𝙱𝙾𝚃 𝙱𝙽𝚆𝙰𝙽𝙴 𝙺 𝙻𝚈 𝙽𝙴𝙲𝙷𝚈 𝙳𝚈𝙴 𝙶𝚈𝙴 𝙻𝙸𝙽𝙺 𝙿𝙰 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙺𝚁𝚈👇👇  😋https://www.facebook.com/sardarjutt.jutt.92?mibextid=kFxxJD`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/2211.jpg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-		api.setMessageReaction("🫡", event.messageID, (err) => {}, true)
+		}
+			<a href="https://ibb.co/HpZskCc"><img src="https://i.ibb.co/bRtZ8F4/IMG-20250103-WA0000.jpg" alt="IMG-20250103-WA0000" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>upload pic</a><br />
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+			}
